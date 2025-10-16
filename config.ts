f7280e3f6f214b8b62398d418164a56281cabb01
Laurent Rossillol <laurent.rossillol@homeserve.fr>
2025-10-16 11:39:46 +0200

    add config

---
+++ config.ts
@@ -0,0 +1,77 @@
+export interface SiteConfig {
+  dir: string;
+  url: string;
+}
+
+export const config: SiteConfig[] = [
+  {
+    dir: './geodis',
+    url: 'https://www.geodis-energies.fr/'
+  },
+  {
+    dir: './chauffage-du-nord',
+    url: 'https://www.chauffage-du-nord.fr/'
+  },
+  {
+    dir: './lepretre',
+    url: 'https://www.lepretre-energies.fr/'
+  },
+  {
+    dir: './concept-habitat',
+    url: 'https://www.concept-habitat-normandie.fr/'
+  },
+  {
+    dir: './id-energies',
+    url: 'https://www.id-energies.com/'
+  },
+  {
+    dir: './jcmconfort',
+    url: 'https://www.jcmconfort.fr/'
+  },
+  {
+    dir: './smt',
+    url: 'https://www.smt-energies.fr/'
+  },
+  {
+    dir: './vbgaz',
+    url: 'https://www.vbgaz.fr/'
+  },
+  {
+    dir: './concept-habitat-normandie',
+    url: 'https://www.concept-habitat-normandie.fr/'
+  },
+  {
+    dir: './conviflamme',
+    url: 'https://www.conviflamme.fr/'
+  },
+  {
+    dir: './mure-energies',
+    url: 'https://www.mure-energies.fr/'
+  },
+  {
+    dir: './aujard',
+    url: 'https://www.aujard.fr/'
+  },
+  {
+    dir: './roussin',
+    url: 'https://www.roussin-energies.fr/'
+  },
+  {
+    dir: './ph-energies',
+    url: 'https://www.ph-energies.fr/'
+  },
+  {
+    dir: './sbf-energies',
+    url: 'https://www.sbf-energies.fr/'
+  },
+  {
+    dir: './sbf-energies',
+    url: 'https://www.sbf-energies.fr/'
+  },
+  {
+    dir: './gd-energies',
+    url: 'https://www.gd-energies.fr/'
+  }
+];
+
+export const valueConfig = 16; 
